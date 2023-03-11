84fd54e4bc45843a45a6ae5c690006e2eab2bdce
Mohammad Javed <101566706+MJaved9@users.noreply.github.com>
2023-03-11 20:19:56 +0530

    Login Signup done Working on Home page and other pages

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import { Heading,Box} from "@chakra-ui/react";
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+  let token=JSON.parse(localStorage.getItem("Token"))
+  let welcome1=JSON.parse(localStorage.getItem("Welcome1"))
+  if(!welcome1){
+    let text="welcome hii this is profile page.."
+    if(window.confirm(text) === true) {
+      text = "You pressed OK!";
+      localStorage.setItem("Welcome1",JSON.stringify("Yes"))
+    } else {
+      text = "You canceled!";
+    }
+  }
+
+  if(token){
+    return (
+      <div>HomePage</div>
+    )
+  }
+  else{
+    return (
+      <Box>
+        <Box p="16">
+          <Heading>Please Login First...</Heading>
+        </Box>
+      </Box>
+    )
+  }
+
 }
 
 export default HomePage
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,80 @@
+import { Box, Heading,Card, CardHeader, CardBody, CardFooter,Image,Stack,Text,Button } from '@chakra-ui/react'
 import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+  // token is use for check user Login or Not if user Login then token save otherwise token is Empty
+  // Profile is for Login user Details
+  let token=JSON.parse(localStorage.getItem("Token"))
+  let profile=JSON.parse(localStorage.getItem("profile"))
+  let welcome=JSON.parse(localStorage.getItem("Welcome"))
+
+  //  Here is for T & C  Logic if user accept then this code will save 
+  // if user cancel then it's show on every onload..
+  if(!welcome){
+    let text="welcome hii this is profile page.."
+    if(window.confirm(text) === true) {
+      text = "You pressed OK!";
+      localStorage.setItem("Welcome",JSON.stringify("Yes"))
+    } else {
+      text = "You canceled!";
+    }
+  }
+ 
+
+
+// here we check if User Login then he can access the page otherwise 
+// he need to Login..
+  if(token){
+    return (
+      <Box>
+        {/* <Box>
+          <Img src=""/>
+        </Box> */}
+        <Box>
+        <Card
+  direction={{ base: 'column', sm: 'row' }}
+  overflow='hidden'
+  variant='outline'
+>
+  {/* I take Dummy profile Image for this.. */}
+  <Image
+    objectFit='cover'
+    maxW={{ base: '100%', sm: '200px' }}
+    src='https://source.unsplash.com/random/200x200?sig=1'
+    alt='Caffe Latte'
+  />
+
+  <Stack>
+    <CardBody>
+      <Heading size='md'>{profile.fullName }</Heading>
+
+      <Text py='2'>
+      email - {profile.email}
+      </Text>
+    </CardBody>
+
+    <CardFooter>
+      User Name - {profile.userName}
+    </CardFooter>
+  </Stack>
+</Card>
+        </Box>
+        <Box>
+
+        </Box>
+      </Box>
+    )
+  }
+  else{
+    return (
+      <div>
+        <Box p="20">
+          <Heading>Please Login First....</Heading>
+        </Box>
+      </div>
+    )
+  }
+
 }
 
 export default Profile
